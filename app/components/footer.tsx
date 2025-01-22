8f12b9e32f3ed3521065780c073d801ba977830c
willzxone <willzxone0987@gmail.com>
2025-01-22 22:43:16 +0500

    feat: enhance footer layout and add Contact Support link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -14,23 +14,42 @@ export const footerIcon: { icons: IconType; path: string }[] = [
 export const footerDocumentation: { name: string; path: string }[] = [
     {name: 'Team', path: '#'},
     {name: 'Documentation', path: '/documentation'},
+    {name: 'Contact Support', path: '/contact'},
 ];
 
 
 const Footer = () => {
   const date = new Date().getFullYear();
   return (
-    <footer className="relative flex flex-col-reverse md:flex-row justify-between items-center w-full min-h-[100px] bg-[#000014] px-4 md:px-8 lg:px-20 py-6 md:py-0 gap-6 md:gap-4 mt-[100px]">
+    <footer className="relative flex flex-col-reverse md:flex-row justify-between items-center w-full min-h-[100px] bg-[#02060D] px-4 md:px-8 lg:px-20 py-6 md:py-0 gap-6 md:gap-4 mt-[100px]">
       {/* Copyright section */}
       <div className="text-center md:text-left">
-        <p className="text-sm md:text-base text-gray-600">
-          copyright@Autoswappr {date}
+        <p className="text-sm md:text-base text-[#E7ECF0]">
+          copyright@Autoswappr{date}
         </p>
       </div>
+      {/* Navigation section */}
 
+      <div className="flex flex-row items-center gap-4 md:gap-4">
+        {footerDocumentation.map((doc, index) => (
+          <div className="flex items-center gap-4">
+          <Link
+            key={index}
+            href={doc.path}
+            className="text-sm md:text-base hover:opacity-80 cursor-pointer"
+          >
+            {doc.name}
+          </Link>
+          {index !== footerDocumentation.length - 1 && <div className=" w-[3px] bg-[#1E2021] h-[12px] rounded-[8px]"></div>}
+
+          </div>
+        ))}
+      </div>
       {/* Social icons section */}
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-4">
         {footerIcon.map((icon, index) => (
+                    <div className="flex items-center gap-4">
+
           <Link
             key={index}
             href={icon.path}
@@ -39,21 +58,13 @@ const Footer = () => {
             <icon.icons />
             {/* w-4 h-4 md:w-5 md:h-5  */}
           </Link>
+          {index !== footerIcon.length - 1 && <div className=" w-[3px] bg-[#1E2021] h-[12px] rounded-[8px]"></div>}
+
+          </div>
         ))}
       </div>
 
-      {/* Navigation section */}
-      <div className="flex flex-row items-center gap-4 md:gap-8">
-        {footerDocumentation.map((doc, index) => (
-          <Link
-            key={index}
-            href={doc.path}
-            className="text-sm md:text-base hover:opacity-80 cursor-pointer"
-          >
-            {doc.name}
-          </Link>
-        ))}
-      </div>
+      
     </footer>
   );
 };
