3883670db877425aea468c258fda534c3b3932e1
“Akshola00” <akinniyishola07@gmail.com>
2025-01-25 17:13:14 +0100

    feat: disconnect modal modified and implemented

--- app/components/disconnect-modal.tsx
+++ app/components/disconnect-modal.tsx
@@ -27,21 +27,23 @@ export default function DisconnectModal({
       className="fixed inset-0 bg-black/60 z-50 backdrop-blur-sm flex items-center justify-center"
       onClick={handleClose}
     >
-      <div className="w-full md:w-[800px] bg-[#08001F] text-center border border-[#170F2E] rounded-3xl py-12 px-6 relative">
-        <button
-          onClick={() => setIsOpen(false)}
-          className="text-gray-400 hover:text-white transition-colors absolute top-8 right-8"
-        >
-          <X className="w-6 h-6" />
-        </button>
-        {/* Modal Header */}
-        <h3 className="text-[18px] lg:text-2xl font-medium text-[#F9F9F9]">
-          Disconnect wallet
-        </h3>
+      <div className="w-full md:w-[800px] bg-[#000103] text-center border border-[#170F2E] rounded-3xl py-12 px-6 relative">
+        <div className="flex justify-between items-center">
+          <p></p>
+          <h3 className="text-[24px] lg:text-2xl font-semibold text-[#F3F5FF]">
+            Disconnect wallet
+          </h3>
+          <button
+            onClick={() => setIsOpen(false)}
+            className="text-gray-400 hover:text-white transition-colors"
+          >
+            <X className="w-6 h-6 text-white" />
+          </button>
+        </div>
 
         {/* Modal Content */}
         <div className="py-[30px] px-[16px] lg:py-[81px] lg:px-[55px] my-6">
-          <p className="text-[#A199B8] text-sm lg:text-base leading-[22px]">
+          <p className="text-[#DCDFE1] text-sm lg:text-lg  leading-[21px]">
             You are disconnecting your wallet from Autoswappr. Are you sure you
             want to continue with this process?
           </p>
@@ -50,13 +52,13 @@ export default function DisconnectModal({
         {/* Modal Actions */}
         <div className="items-center grid grid-cols-[1fr_1fr] gap-x-4">
           <button
-            className="py-3 text-sm lg:text-base lg:py-4 rounded-full hover:bg-blue-700 bg-[#100827] text-white transition-colors"
+            className="py-3 text-sm lg:text-lg lg:py-4 rounded-[8px] hover:bg-blue-700 border-2 border-[#1E2021] text-white transition-colors"
             onClick={handleClose}
           >
             Cancel
           </button>
           <button
-            className="py-3 text-sm lg:text-base lg:py-4 rounded-full bg-blue-600 hover:bg-blue-700 text-white transition-colors"
+            className="py-3 text-sm lg:text-lg lg:py-4 rounded-[8px] bg-blue-600 hover:bg-blue-700 text-white transition-colors"
             onClick={handleDisconnect}
           >
             Yes, Disconnect
