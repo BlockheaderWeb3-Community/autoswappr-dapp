ff082c4d79a4de9f279f84232fe857366ea69c92
“Akshola00” <akinniyishola07@gmail.com>
2025-01-26 17:04:35 +0100

    feat: implement new grant permission modal

--- app/components/grant-permission-modal.tsx
+++ app/components/grant-permission-modal.tsx
@@ -1,5 +1,6 @@
 import { X } from "lucide-react";
 import React from "react";
+import GenericModal from "./generic-modal";
 
 function GrantPermissionModal({
   handleClose,
@@ -14,19 +15,15 @@ function GrantPermissionModal({
       onClick={handleClose}
     >
       <div
-        className="w-full max-w-[800px] px-4 md:px-[60px] md:py-[70px]  py-12 border border-[#170F2E] bg-[#08001F]  text-center rounded-xl relative"
+        className="w-full max-w-[800px] px-4 md:px-[60px] md:py-[70px]  py-12 border bg-[#02060D] border-none text-center rounded-xl relative"
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
-        <button className="absolute top-6 right-6" onClick={handleClose}>
-          <X className="cursor-pointer" />
-        </button>
-
-        <h2 className="font-semibold text-2xl leading-8 text-[#F9F9F9]">
+        <GenericModal handleClose={() => {handleClose()}} >
           Give Autoswappr Permission
-        </h2>
-        <p className="text-base leading-[22px] text-[#A199B8] py-[60px] px-[55px] my-6">
+        </GenericModal>
+        <p className="text-base leading-[22px] text-[#DCDFE1] py-[60px] px-[55px] my-6">
           By clicking the &apos;Continue&apos; button, you are giving Autoswappr
           access to spend your tokens as soon as they get into your wallet, so
           they can automatically be swapped to your preferred stable base token.
@@ -34,13 +31,13 @@ function GrantPermissionModal({
         </p>
         <div className="grid grid-cols-2 gap-x-6 items-center justify-center text-base leading-[22px] text-[#F9F9F9]">
           <button
-            className="py-5 bg-[#100827] rounded-full"
+            className="py-5 bg-[#0D1016] rounded-[8px] border-[1px] border-[#1E2021]"
             onClick={handleClose}
           >
             Cancel
           </button>
           <button
-            className="rounded-full bg-[#0F96E3] py-5"
+            className="bg-[#0F96E3] py-5 rounded-[8px] border-[1px] border-[#1E2021]"
             onClick={handleSubmit}
           >
             Yes, Continue
