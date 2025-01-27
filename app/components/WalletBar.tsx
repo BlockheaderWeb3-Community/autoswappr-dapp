15de19b5bfb93bd7d4ab14f41f283f9039e78ba8
Oshioke-Salaki <salaki1902@gmail.com>
2025-01-27 13:15:01 +0100

    fix: tweek navbar styles

--- app/components/WalletBar.tsx
+++ app/components/WalletBar.tsx
@@ -2,11 +2,7 @@
 import { useAccount } from "@starknet-react/core";
 import Address from "./address";
 
-function WalletBar({
-  toggleModal,
-}: {
-  toggleModal: () => void;
-}) {
+function WalletBar({ toggleModal }: { toggleModal: () => void }) {
   const { address } = useAccount();
 
   return (
@@ -16,7 +12,7 @@ function WalletBar({
       ) : (
         <button
           onClick={toggleModal}
-          className="p-3 md:p-[16px] w-[204px] md:w-[230px] bg-[#02060D1F] border border-[#135DA3] rounded-lg text-sm md:text-base leading-5 text-[#F3F5FF] font-semibold "
+          className="p-3 md:py-[12px] md:px-7 bg-[#0D10163D] border border-[#135DA3] rounded-lg text-sm leading-6 text-[#F3F5FF] font-semibold "
         >
           Connect Wallet
         </button>
