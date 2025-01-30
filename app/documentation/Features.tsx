e21f5cdcb8d77fb9ff29408b3d500ec9bcc05d71
MullerTheScientist <163424834+MullerTheScientist@users.noreply.github.com>
2025-01-29 18:55:47 -0800

    Updated Features.tsx

--- app/documentation/Features.tsx
+++ app/documentation/Features.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 import getHero from "../../public/featureTop.png"
 import getMid from "../../public/featureEnd.png"
 
-  
 export default function Features(){
     return <div className="w-full relative bg-[#08001F]">   
     <Image src={getHero} alt="" className="w-full py-5" />
 
    
     <div className="grid gap-5 font-normal leading-6 text-sm">
-        <h3 className="font-semibold text-sm leading-6">Swapping Tokens Automatically</h3>
-        <ol className="list-disc text-[#D2CED8CC] pl-4">
+        <h3 className="font-semibold text-white text-sm leading-6">Swapping Tokens Automatically</h3>
+        <ol className="list-disc font-[400] text-[14px] text-[#DCDFE1] pl-4">
             <li> How It Works: When a token is received in your connected wallet, Autoswappr automatically exchanges it for your selected stable token also known as base token (e.g., USDT or USDC) using the best available rate.</li>
             <li>Customisation: Users can define thresholds for automatic swaps or choose to swap all tokens.</li>
             <li>Supported Blockchains: Ethereum, Binance Smart Chain, and Polygon.</li>
@@ -19,8 +18,8 @@ export default function Features(){
     </div>
     <Image src={getMid} alt="" className="w-full py-5" />
     <div className="grid gap-2 font-normal leading-6 text-sm">
-        <h3 className="font-semibold text-sm leading-6">DEX Page</h3>
-        <ol className="list-disc text-[#D2CED8CC] pl-4">
+        <h3 className="font-semibold text-sm leading-6 text-[#DCDFE1]">DEX Page</h3>
+        <ol className="list-disc font-[400] text-[14px] text-[#DCDFE1] pl-4">
             <li> Simple Interface: The DEX page allows you to swap tokens manually. Enter the token pair, confirm the rate, and complete your swap in seconds.
             </li>
             <li>Competitive Fees: Enjoy some of the lowest transaction fees in the market.</li>
