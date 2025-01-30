162c81a455fe33db3190dca1575346260c37125e
MullerTheScientist <163424834+MullerTheScientist@users.noreply.github.com>
2025-01-29 18:56:41 -0800

    Updated GetStarted.tsx

--- app/documentation/GetStarted.tsx
+++ app/documentation/GetStarted.tsx
@@ -3,14 +3,13 @@ import getHero from "../../public/getting-started-hero.png";
 import getMid from "../../public/getting-started-mid.png";
 import getBottom from "../../public/getting-started-bottom.png";
 
- 
 export default function GetStarted() {
   return (
     <div className="w-full relative bg-[#08001F]">
       <Image src={getHero} alt="" className="w-full py-5" />
       <div className="grid gap-5 font-normal leading-6 text-sm">
-        <h3 className="font-semibold text-sm leading-6">Creating an Account</h3>
-        <ol className="list-decimal text-[#D2CED8CC] pl-4">
+        <h3 className="font-semibold text-sm leading-6 text-[#DCDFE1]">Creating an Account</h3>
+        <ol className="list-decimal font-[400] text-[14px] text-[#DCDFE1] pl-4">
           <li>Visit autoswapper.io</li>
           <li>Click the &apos; Connect Wallet &apos &apos; button in the top-right corner</li>
           <li>Select a wallet to connect to Autoswapper</li>
@@ -18,25 +17,25 @@ export default function GetStarted() {
         </ol>
       </div>
       <Image src={getMid} alt="" className="w-full py-5 object-cover" />
-      <div className="grid gap-2 font-normal leading-6 text-sm">
+      <div className="grid gap-2 font-normal leading-6 text-[#DCDFE1] text-sm">
         <h3 className="font-semibold text-sm leading-6">
           Connecting Your Wallet
         </h3>
-        <p className="text-[#D2CED8CC]">
+        <p className="text-[#DCDFE1]">
           Autoswappr supports the following wallets:
         </p>
-        <ol className="list-decimal text-[#D2CED8CC] pl-4">
+        <ol className="list-decimal font-[400] text-[14px] text-[#DCDFE1] pl-4">
           <li>Argent</li>
           <li>Braavos</li>
         </ol>
       </div>
 
       <Image src={getBottom} alt="" className="w-full py-5" />
-      <div className="grid gap-5 font-normal leading-6 text-sm">
+      <div className="grid gap-5 font-normal text-[#DCDFE1] leading-6 text-sm">
         <h3 className="font-semibold text-sm leading-6">
           To connect your wallet
         </h3>
-        <ol className="list-decimal text-[#D2CED8CC] pl-4">
+        <ol className="list-decimal font-[400] text-[14px] text-[#DCDFE1] pl-4">
           <li>Click the Connect Wallet button on the homepage.</li>
           <li>Choose your wallet provider from the list.</li>
           <li>Confirm the connection through your wallet app.</li>
