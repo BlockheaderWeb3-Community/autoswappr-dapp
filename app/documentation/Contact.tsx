1df76da19e3e7bd530b928a2a87ed5df608022d0
MullerTheScientist <163424834+MullerTheScientist@users.noreply.github.com>
2025-01-29 18:52:25 -0800

    Updated Contact.tsx to reflect the figma color

--- app/documentation/Contact.tsx
+++ app/documentation/Contact.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="grid gap-5 font-normal leading-6 text-sm">
       <h3 className="font-semibold text-sm leading-6">Community</h3>
-      <ol className="list-decimal text-[#D2CED8CC] pl-4">
+     <ol className="list-decimal text-[#D2CED8CC] pl-4 font-[400] text-[14px]">
         <li>
           Twitter: <a href="https://twitter.com/auto_swappr">Join Here</a>
         </li>
